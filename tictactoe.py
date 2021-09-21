677655075340b23755f7caf9c2762a0dee91d1ec
vineelgajjala <gajjala.vineel@gmail.com>
2021-09-21 12:34:22 -0500

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,27 @@
+def insertBoard(letter, pos):
+    pass
+
+def spaceIsFree(pos):
+    pass
+
+def isWinner(bo, le):
+    pass
+
+def playerMove():
+    pass
+
+def selectRandom(li):
+    pass
+    
+def compMove():
+    pass
+    
+def isBoardFull(board):
+    pass
+
+def printBoard():
+    pass
+
+def main():
+    pass
+    
